d86121e202cb5cdc77832a2a2630d199b37d4761
John <jhallman5@gmail.com>
2017-11-15 14:41:50 -0800

    Completes a working implementation of retailer_affinity

--- infoscout/model.py
+++ infoscout/model.py
@@ -1,16 +1,16 @@
 import requests
+import pandas as pd
 
-def generate_brand_data():
-    """Returns the number of sales for each brand at each retailer."""
-    AWSjson = requests.get('https://s3.amazonaws.com/isc-isc/trips_gdrive.json').json()
-    brands = {item['brand']: {} for item in AWSjson}
-    for item in AWSjson:
-        if(brands[item['brand']].get(item["retailer"], None)):
-            brands[item['brand']][item["retailer"]] += int(item["qty"])
-        else:
-            brands[item['brand']][item["retailer"]] = int(item["qty"])
-    print(brands)
-    return brands
+# def generate_brand_data():
+#     """Returns the number of sales for each brand at each retailer."""
+#     AWSjson = requests.get('https://s3.amazonaws.com/isc-isc/trips_gdrive.json').json()
+#     brands = {item['brand']: {} for item in AWSjson}
+#     for item in AWSjson:
+#         if(brands[item['brand']].get(item["retailer"], None)):
+#             brands[item['brand']][item["retailer"]] += int(item["qty"])
+#         else:
+#             brands[item['brand']][item["retailer"]] = int(item["qty"])
+#     return brands
 
 def generate_retailer_data():
     """Returns the number of sales for each retailer per brand."""
@@ -21,17 +21,23 @@ def generate_retailer_data():
             retailers[item['retailer']][item["brand"]] += int(item["qty"])
         else:
             retailers[item['retailer']][item["brand"]] = int(item["qty"])
-    print(retailers['Walmart'])
     return retailers
 
-# def generate_percent_per_retailer(retailerData):
-#     """Returns the sale percentages per brand for each retailer."""
-#     for retailer in retailerData:
-#         for brand in retailer:
-#             print(brand)
-#     # totalSales = {item['retailer'] : {} for item in retailerData}
-#     return 1
-#
+def generate_percent_sales_table(retailerData):
+    """Returns the sale percentages per brand for each retailer."""
+    df = pd.DataFrame(retailerData)
+    for column in df:
+        new_df = df.agg(column).apply(lambda x: percentages(x, df[column]))
+        df.update(new_df)
+    return df
 
-# def retailer_affinity(focus_brand):
-# """Returns the strongest retailer affinity relative to other brands."""
+def percentages(item, retailer):
+    """Generates percentage of item sales per retailer."""
+    interFloat =  100 * item /float(retailer.sum())
+    return round(interFloat, 2)
+
+def retailer_affinity(focus_brand):
+    """Returns the strongest retailer affinity relative to other brands."""
+    data = generate_retailer_data()
+    df = generate_percent_sales_table(data)
+    return df.ix[focus_brand].idxmax(axis=1)
--- infoscout/server.py
+++ infoscout/server.py
@@ -1,14 +1,10 @@
 from flask import Flask, render_template
-from model import generate_brand_data, generate_retailer_data
+from model import generate_retailer_data, generate_percent_sales_table, retailer_affinity
 import requests
-import json
 import pandas as pd
 import matplotlib
-from matplotlib.pyplot import pie, axis, show
-import numpy as np
 
 matplotlib.use('TkAgg')
-
 
 app=Flask(__name__)
 
@@ -19,24 +15,26 @@ def hello():
 
 @app.route("/affinity")
 def goodbye():
-    # generate_brand_data()
-    return "goodbye"
+    data = generate_retailer_data()
+    df = generate_percent_sales_table(data)
+    return render_template('template.html',tables=[df.to_html()], titles = ['na', 'Percent Sales'])
 
 @app.route("/affinity2")
 def goodbye2():
     data = generate_retailer_data()
     df = pd.DataFrame(data)
-    df_og = pd.DataFrame(data)
     for column in df:
         new_df = df.agg(column).apply(lambda x: percentages(x, df[column]))
         df.update(new_df)
-    return render_template('template.html',tables=[df_og.to_html(), df.to_html()], titles = ['na', 'Raw Sales', 'Percent Sales'])
+    return render_template('template.html',tables=[df.to_html()], titles = ['na', 'Percent Sales'])
 
+@app.route("/affinity3")
+def find_affinity():
+    return retailer_affinity('Monster')
 
 def percentages(item, retailer):
      interFloat =  100 * item /float(retailer.sum())
      return round(interFloat, 2)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
